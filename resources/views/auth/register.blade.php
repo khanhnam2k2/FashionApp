fdcfc51cd9ecbcffa81ed3576bbca6b4488a8343
bule1794 <75290077+bule1794@users.noreply.github.com>
2023-10-06 13:49:24 +0700

    fix logic register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,17 +13,32 @@
                             @csrf
                             <div class="mb-3">
                                 <label for="name" class="form-label">Name</label>
-                                <input type="text"name="name" class="form-control" id="name"
-                                    aria-describedby="textHelp">
+                                <input type="text"name="name" class="form-control" id="name">
+                                @error('name')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="email" class="form-label">Email Address</label>
-                                <input type="email" name="email" class="form-control" id="email"
-                                    aria-describedby="emailHelp">
+                                <input type="email" name="email" class="form-control" id="email">
+                                @error('email')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
                             </div>
                             <div class="mb-4">
                                 <label for="password" class="form-label">Password</label>
                                 <input type="password" name="password" class="form-control" id="password">
+                                @error('password')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="mb-4">
+                                <label for="password_confirmation" class="form-label">Confirm Password</label>
+                                <input type="password" name="password_confirmation" class="form-control"
+                                    id="password_confirmation">
+                                @error('password_confirmation')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
                             </div>
                             <button class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Sign Up
                             </button>
