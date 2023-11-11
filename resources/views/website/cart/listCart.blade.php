7befe3f4c7a3f9669c30585a28d6af360654275f
bule1794 <75290077+bule1794@users.noreply.github.com>
2023-11-11 16:11:17 +0700

    fix count cart item

--- resources/views/website/cart/listCart.blade.php
+++ resources/views/website/cart/listCart.blade.php
@@ -1,6 +1,6 @@
 @if (count($cartItemLimit) > 0)
     <div class="w-100">
-        <h6 class="mb-3">4 Sản phẩm mới thêm</h6>
+        <h6 class="mb-3">{{ count($cartItemLimit) }} Sản phẩm mới thêm</h6>
         @foreach ($cartItemLimit as $item)
             <div class="d-flex justify-content-between align-items-center mb-3 w-100">
                 <div class="">
