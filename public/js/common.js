c0c95058a45cb6ee33f569fc558bb2161f89eb99
bule1794 <75290077+bule1794@users.noreply.github.com>
2023-11-12 14:01:11 +0700

    add function debounce

--- public/js/common.js
+++ public/js/common.js
@@ -111,3 +111,24 @@ function addToCart(productId, quantity, size, btn) {
         btn.prop('disabled', false);
     })
 }
+
+/**
+ * debounce function
+ * @param {Function} func callback function
+ * @param {Number} wait waiting time
+ * @param {boolean} immediate 
+ */
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
